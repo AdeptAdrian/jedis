1e40d7df6538af7fbbcec8ac4d89aa7b6fab6c68
Adrian <adrian.01@list.ru>
2024-09-08 18:36:37 -0500

    Changed to multi-threading

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,23 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
 public class Main {
   public static void main(String[] args) {
-    //Server side connection
+    // Server side connection
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
-    //Hard code the port for now
+    // Hard code the port for now
     int port = 3003;
     try {
       serverSocket = new ServerSocket(port);
       serverSocket.setReuseAddress(true);
       // Wait for connection from client
-      clientSocket = serverSocket.accept();
-      processTheClient(clientSocket);
+      while (true) {
+        clientSocket = serverSocket.accept();
+        processTheClient(clientSocket);
+      }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
@@ -32,25 +31,10 @@ public class Main {
     }
   }
 
+  //Thread to handle the client
   public static void processTheClient(Socket client) {
-    // Handle the client's requests
-    BufferedReader input;
-    BufferedWriter output;
-    // Read from/write to client
-    try {
-      input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-      output = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-      String buffer = "";
-      while ((buffer = input.readLine()) != null) {
-        System.out.println("::" + buffer);
-        if (buffer.equalsIgnoreCase("ping")) {
-          output.write("+PONG\r\n");
-          output.flush();
-        }
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    ClientThread thclient = new ClientThread(client);
+    thclient.start();
   }
 
 }
